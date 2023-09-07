f46e6a06a6536725a8c5d8f928b8b889cc3a5d82
lsb0120 <c32354518@gmail.com>
2023-09-07 13:41:29 +0900

    1223

---
+++ app/src/main/java/com/example/mybookmark/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mybookmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mybookmark.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding; //맴버변수
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding= ActivityMainBinding.inflate(getLayoutInflater());//클래스 구조로 띄우기
+        
+
+
+
+        binding.
+
+
+    }
+}
