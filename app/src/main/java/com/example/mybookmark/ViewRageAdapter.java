359bf7f2a18330efb2b387216388f9515299aace
lsb0120 <c32354518@gmail.com>
2023-09-07 14:59:00 +0900

    add

---
+++ app/src/main/java/com/example/mybookmark/ViewRageAdapter.java
@@ -0,0 +1,39 @@
+package com.example.mybookmark;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewRageAdapter extends FragmentStateAdapter
+{
+    public ViewRageAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if(position==0){
+            //Fragment_1 f1=new Fragment_1();
+            WebViewFragment f1=new WebViewFragment("https://m.naver.com/");
+            return f1;
+        }else if (position==1){
+            //Fragment_2 f2=new Fragment_2();
+            WebViewFragment f2=new WebViewFragment("https://www.google.com/");
+            return f2;
+        }else if (position==2){
+            Fragment_3 f3=new Fragment_3();
+            return f3;
+        }
+
+
+
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3; //중요
+    }
+}
